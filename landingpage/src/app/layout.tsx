748b3a6c9158dd1cf723ca7d05e9ca01c48530f9
rubandeveloper <rajajayarubant@gmail.com>
2025-08-30 22:37:40 +0530

    Admin Changes

--- landingpage/src/app/layout.tsx
+++ landingpage/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import LoaderUi from "../components/ui/LoaderUi";
 
 import type { Metadata } from "next";
@@ -28,6 +29,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t30snu1yx7");
+          `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Suspense fallback={<LoaderUi props={{}} />}>
